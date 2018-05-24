d62559639309e65eba8a0fec50fa429218b0f6d8
Ernesto Alejo <ernestokarim@gmail.com>
2018-05-24 21:08:37 +0200

    Configura el logging de depuración automáticamente al abrir la aplicación.

---
+++ logging.go
@@ -0,0 +1,14 @@
+package services
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	if IsLocal() {
+		log.SetFormatter(&log.TextFormatter{
+			ForceColors: true,
+		})
+		log.SetLevel(log.DebugLevel)
+	}
+}
